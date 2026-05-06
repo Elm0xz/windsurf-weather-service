904c4332146ea4549ec7e0b6e764c181afbafd32
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-05-06 14:46:27 +0200

    [WIND-1] infra: initial commit

---
+++ src/main/java/com/pretz/windsurf/WindsurfWeatherApp.java
@@ -0,0 +1,13 @@
+package com.pretz.windsurf;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class WindsurfWeatherApp {
+
+    static void main() {
+        run(WindsurfWeatherApp.class);
+    }
+}
